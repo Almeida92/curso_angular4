d0f222536474923925096e227166f240b492bd74
Felipe Almeida <felipepereira937@gmail.com>
2017-11-11 13:19:57 -0200

    alguns testes Typescript

--- src/app/meu-primeiro/meu-primeiro.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'meu-primeiro-component',
-    template: `
-    <p> Meu Primeiro component com Angular !</p>
-    `
-})
-export class MeuPrimeiroComponent {}
--- src/app/meu-primeiro2/meu-primeiro2.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, ViewEncapsulation } from '@angular/core';
-
-@Component({
-  selector: 'meu-primeiro2-component',
-  templateUrl: './meu-primeiro2.component.html',
-  styleUrls: ['./meu-primeiro2.component.css'],
-  encapsulation: ViewEncapsulation.None
-})
-export class MeuPrimeiro2Component {}
