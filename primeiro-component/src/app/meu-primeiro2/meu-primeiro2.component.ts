3b2af01edcd4f7fb4813d7d686783494237881ed
Felipe Almeida <felipepereira937@gmail.com>
2017-11-11 13:05:25 -0200

    movendo primeiro component

---
+++ primeiro-component/src/app/meu-primeiro2/meu-primeiro2.component.ts
@@ -0,0 +1,9 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'meu-primeiro2-component',
+  templateUrl: './meu-primeiro2.component.html',
+  styleUrls: ['./meu-primeiro2.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class MeuPrimeiro2Component {}
